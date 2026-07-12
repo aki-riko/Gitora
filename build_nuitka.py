7d31a23b4e855be20f5460a5cd9c122dd5e43336
aki-riko <aki-riko@users.noreply.github.com>
2026-07-12 23:13:58 +0800

    chore: 发布 v1.2.2

--- build_nuitka.py
+++ build_nuitka.py
@@ -58,7 +58,7 @@ args = [
     # 产品元信息
     "--company-name=aki-riko",
     "--product-name=Gitora",
-    "--product-version=1.2.1",
+    "--product-version=1.2.2",
     "--file-description=Gitora - Git GUI",
     f"--output-dir={OUT}",
     ENTRY,
