6681d2c8e019d9f864d6bd290c8eb44170fde018
aki-riko <aki-riko@users.noreply.github.com>
2026-07-12 19:59:32 +0800

    chore: bump version to v1.2.1

--- build_nuitka.py
+++ build_nuitka.py
@@ -58,7 +58,7 @@ args = [
     # 产品元信息
     "--company-name=aki-riko",
     "--product-name=Gitora",
-    "--product-version=1.2.0",
+    "--product-version=1.2.1",
     "--file-description=Gitora - Git GUI",
     f"--output-dir={OUT}",
     ENTRY,
