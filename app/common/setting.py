ab10579ba3f360094e133e49914df07e152569c0
aki-riko <aki-riko@users.noreply.github.com>
2026-07-15 13:06:39 +0800

    chore: 发布 v1.2.6

--- app/common/setting.py
+++ app/common/setting.py
@@ -10,7 +10,7 @@ DEBUG = "__compiled__" not in globals()
 
 YEAR = 2025
 AUTHOR = "aki-riko"
-VERSION = "v1.2.5"
+VERSION = "v1.2.6"
 APP_NAME = "Gitora"
 APP_USER_MODEL_ID = "PrismQML.Gitora"
 # 项目地址
